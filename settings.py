a7d9cf9b52a12d3238c7125ab9999890f0b69194
Faisal Lalani <flalani@ip-10-0-2-160.ap-south-1.compute.internal>
2021-06-14 15:23:07 +0000

    added README and requirements.txt

--- settings.py
+++ settings.py
@@ -1,4 +1,12 @@
+# The name of the form to run the algorithm on.
+# Ex. flu_check
 FORM = ''
+# The full path where the form is located.
+# Ex. 'data/forms/' + FORM + '.gzip'
 FORM_PATH = ''
+# The full path where the form summary is located.
+# Ex. 'data/forms/' + FORM + '.csv'
 FORM_SUMMARY_PATH = ''
+# The full path where the form annotations are located.
+# Ex. 'data/annotations/' + FORM + '.csv'
 FORM_ANNOTATIONS_PATH = ''
