11ff0dd00e0372a191babf9ce61ea806086b483a
Faisal Lalani <flalani@ip-10-0-2-160.ap-south-1.compute.internal>
2021-09-16 19:23:30 +0000

    changed priority icons

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-document.body.style.backgroundColor = 'blue';
+function toggleArrow(txt) {
+    if(btn.innerText == '▼') {
+        btn.innerText = '▲'
+    } else {
+        btn.innerText = '▼'
+    }
+}
+
+$('#collapseOne').on('shown.bs.collapse', function () {
+    toggleArrow(this);
+})
+
+$('#collapseTwo').on('shown.bs.collapse', function () {
+    toggleArrow(this);
+})
+
+$('#collapseThree').on('shown.bs.collapse', function () {
+    toggleArrow(this);
+})
+
+$('#collapseFour').on('shown.bs.collapse', function () {
+    toggleArrow(this)
+});
